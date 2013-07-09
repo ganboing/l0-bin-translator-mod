651a431b0cac629b2b4877e8e46a954994f80813
ganboing <ganboing@gmail.com>
2013-07-09 20:23:29 +0800

    mod

--- testjit.c
+++ testjit.c
@@ -5,9 +5,12 @@
  *      Author: PROGMAN
  */
 
+#include <unistd.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
 #define FUNC_SIZE 0x10
 
@@ -21,8 +24,10 @@ void functobecopied(void)
 
 int main()
 {
+	unsigned long pagesize = sysconf(_SC_PAGE_SIZE);
 	void* func_runtime = malloc(FUNC_SIZE);
 	memcpy(func_runtime, functobecopied,FUNC_SIZE );
+	mprotect((((uint64_t)func_runtime)&(~(pagesize-1))), (FUNC_SIZE + pagesize - 1), PROT_READ | PROT_WRITE | PROT_EXEC);
 	(*((FuncType)func_runtime))();
 	return 0;
 }
