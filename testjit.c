8e09012ec58025f47e46d1c83f19bf9391d8dd21
ganboing <ganboing@gmail.com>
2013-07-09 19:54:09 +0800

    mod

--- testjit.c
+++ testjit.c
@@ -5,4 +5,24 @@
  *      Author: PROGMAN
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define FUNC_SIZE 0x10
+
+typedef void (*FuncType)(void);
+
+void functobecopied(void)
+{
+	printf("this is a function\n");
+	return;
+}
+
+int main()
+{
+	void* func_runtime = malloc(FUNC_SIZE);
+	memcpy(func_runtime, functobecopied,FUNC_SIZE );
+	(*((FuncType)func_runtime))();
+	return 0;
+}
