16c394dfe0924ccdfe600d3ba13ce7fb0dbac90b
ganboing <ganboing@gmail.com>
2013-07-12 19:31:59 +0800

    mod

--- DecodeI0.h
+++ DecodeI0.h
@@ -8,6 +8,13 @@
 #ifndef DECODEI0_H_
 #define DECODEI0_H_
 
+#include <stdint.h>
 
+typedef struct _DECODE_STATUS{
+	unsigned long status;
+	unsigned long detail;
+}DECODE_STATUS;
+
+DECODE_STATUS TranslateI0ToNative(uint8_t** spc, uint8_t** tpc, uint8_t* nativelimit, uint8_t* i0limit, unsigned int is_write) ;
 
 #endif /* DECODEI0_H_ */
