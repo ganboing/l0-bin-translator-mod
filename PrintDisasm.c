e8e410d13be9f8a612ac086efe656b0f4097d976
ganboing <ganboing@gmail.com>
2013-07-18 13:31:40 +0800

    mod

--- PrintDisasm.c
+++ PrintDisasm.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 void InitTestTransOutput(void) __attribute__ ((constructor));
 
 static int outputfile = -1;
+
+static const unsigned long suggest_map_to_addr = 0x500000000000ULL;
+
+void* real_map_addr = 0;
 
 void InitTestTransOutput(void)
 {
@@ -14,5 +19,11 @@ void InitTestTransOutput(void)
 	if(outputfile == -1)
 	{
 		exit(-100);
+	}
+	real_map_addr = mmap(suggest_map_to_addr, (1024ULL*1024ULL),PROT_READ|PROT_WRITE,
+			MAP_SHARED, outputfile, 0);
+	if(real_map_addr < (0x400000000000ULL))
+	{
+		exit(-200);
 	}
 }
