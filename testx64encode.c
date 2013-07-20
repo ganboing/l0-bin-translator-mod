f90a71b49c799bed2d1be0d8f7d02ec87782de93
ganboing <ganboing@gmail.com>
2013-07-20 23:35:53 +0800

    mod

--- testx64encode.c
+++ testx64encode.c
@@ -99,6 +99,7 @@ __inline static uint8_t MAP_TO_NATIVE_REGISTER(uint64_t i0addr)
 
 DECODE_STATUS TranslateBIJ(I0INSTR* i0instr, uint8_t* nativeblock, uint64_t* nativelimit, int is_write)
 {
+	printf("native limit now @ %lx\n", (*nativelimit));
 	//ModLog("bij: opr val.v64 == %lx addrm == %x disp = %x\n",i0instr->opr[0].val.v64, i0instr->opr[0].addrm, i0instr->opr[0].disp32);
 	//25 ?? ?? ?? ??		andl $0x(cnt_of_ijtab-1), %eax 
 	//c1 e0 03				shll $0x03, %eax
