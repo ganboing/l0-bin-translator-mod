ad2290b9df944648c86f9a81f27998295d22c39d
progman <progman@ubuntu>
2014-03-07 11:47:32 +0800

    mod

--- src/scan_func.cpp
+++ src/scan_func.cpp
@@ -1,24 +1,355 @@
 #include <iostream>
 #include <cstdlib>
 #include <fcntl.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include "scan_func.hpp"
+#include <boost/shared_ptr.hpp>
 
-int main(int argc, char** argv)
-{
+#define Op1 (opnds[0])
+#define Op2 (opnds[1])
+#define Op3 (opnds[2])
+#define Op4 (opnds[3])
+#define Op5 (opnds[4])
+
+unsigned const i0_Ins_Opnd_Cnt[] = { 2, //conv
+		3, //add
+		3, //sub
+		3, //mul
+		3, //div
+		3, //and
+		3, //or
+		3, //xor
+		4, //spawn
+		5, //spawnx
+		0, //exit
+		3, //bcc
+		2, //bcz
+		1, //bj
+		1, //bij
+		0, //nop
+		1, //int
+		3, //shl
+		3, //shr
+		4, //scmp
+		2, //mov
+		};
+static_assert((qnumber(i0_Ins_Opnd_Cnt) == I0_ins_last_ins), "i0 ins opt number illegal");
+
+//static_assert((sizeof(op_t) == (2+2+4+8)), "check op_t size");
+
+static uint8_t i0_get_byte(i0_ea_type_t ea) {
+	return 0;
+}
+
+static uint16_t i0_get_word(i0_ea_type_t ea) {
+	return 0;
+}
+
+static uint32_t i0_get_dword(i0_ea_type_t ea) {
+	return 0;
+}
+
+static uint64_t i0_get_qword(i0_ea_type_t ea) {
+	return 0;
+}
+
+i0_opcode4_t::i0_opcode4_t(insn_t& _cmd) :
+		insn_ref(_cmd), shift_cnt(0) {
+	load_op_bytes();
+}
+
+void i0_opcode4_t::load_op_bytes() {
+	load_bytes(2);
+}
+
+void i0_opcode4_t::load_extra_bytes(uint8_t cnt) {
+	load_bytes(cnt - 2);
+}
+
+void i0_opcode4_t::load_bytes(uint8_t cnt) {
+	uint8_t load_cnt = insn_ref.size + cnt;
+	while (cnt) {
+		opcode4.b[3 - load_cnt + cnt] = insn_ref.i0_fetch_byte();
+		--cnt;
+	}
+}
+uint32_t i0_opcode4_t::get_bits(unsigned start, unsigned len) {
+	return ((opcode4.l << start) >> (32 - len));
+}
+uint32_t i0_opcode4_t::fetch_bits(unsigned len) {
+	uint32_t ret = get_bits(shift_cnt, len);
+	shift_cnt += len;
+	return ret;
+}
+
+void insn_t::fill_oper(op_t& op) {
+
+}
+
+void insn_t::fill_oper(op_t& op, uint8_t attr) {
+
+}
+
+insn_t::insn_t(i0_ea_type_t _ip)
+try :
+		opnds(NULL), ip(_ip), op_name(I0_ins_last_ins), opt(
+				i0_ins_opt_pref_last), ins_attr(i0_attr_last), size(0) {
+	i0_opcode4_t opcode4(*this);
+	uint32_t opcode = opcode4.fetch_bits(I0_INS_BIT_LEN_OPCODE);
+	switch (opcode) {
+	case I0_OPCODE_CONV:
+		op_name = I0_ins_conv;
+		opcode4.load_extra_bytes(I0_INS_LEN_CONV);
+		src_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+		dest_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+		if (src_attr == dest_attr) {
+			op_name = I0_ins_mov;
+			ins_attr = src_attr;
+		}
+		Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		fill_oper(Op1, src_attr);
+		fill_oper_M(Op2, dest_attr);
+		return;
+	case I0_OPCODE_ADD:
+		op_name = I0_ins_add;
+		break;
+	case I0_OPCODE_SUB:
+		op_name = I0_ins_sub;
+		break;
+	case I0_OPCODE_MUL:
+		op_name = I0_ins_mul;
+		break;
+	case I0_OPCODE_DIV:
+		op_name = I0_ins_div;
+		break;
+	case I0_OPCODE_AND:
+		op_name = I0_ins_and;
+		break;
+	case I0_OPCODE_OR:
+		op_name = I0_ins_or;
+		break;
+	case I0_OPCODE_XOR:
+		op_name = I0_ins_xor;
+		break;
+	case I0_OPCODE_SPAWN:
+		op_name = I0_ins_spawn;
+		opcode4.load_extra_bytes(I0_INS_LEN_SPAWN);
+		Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op3.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op4.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		fill_oper_M(Op1);
+		fill_oper_M(Op2);
+		fill_oper_M(Op3);
+		fill_oper_M(Op4);
+		return;
+	case I0_OPCODE_EXIT:
+		op_name = I0_ins_exit;
+		opcode4.load_extra_bytes(I0_INS_LEN_EXIT);
+		{
+			uint32_t exit_opt = opcode4.fetch_bits(I0_INS_BIT_LEN_OPT_EXIT);
+			switch (exit_opt) {
+			case I0_OPT_EXIT_A:
+				opt = i0_ins_opt_pref_exit_a;
+				break;
+			case I0_OPT_EXIT_AD:
+				opt = i0_ins_opt_pref_exit_ad;
+				break;
+			case I0_OPT_EXIT_C:
+				opt = i0_ins_opt_pref_exit_c;
+				break;
+			case I0_OPT_EXIT_CD:
+				opt = i0_ins_opt_pref_exit_cd;
+				break;
+			}
+		}
+		return;
+	case I0_OPCODE_B: {
+		uint32_t b_opt = opcode4.fetch_bits(I0_INS_BIT_LEN_OPT_B);
+		switch (b_opt) {
+		case I0_OPT_B_IJ:
+			op_name = I0_ins_bij;
+			opcode4.load_extra_bytes(I0_INS_LEN_BIJ);
+			Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+			fill_oper_C_indir(Op1);
+			return;
+		case I0_OPT_B_J:
+			op_name = I0_ins_bj;
+			opcode4.load_extra_bytes(I0_INS_LEN_BJ);
+			opt = opcode4.fetch_bits(I0_INS_BIT_LEN_RA);
+			fill_oper_C(Op1);
+			return;
+		case I0_OPT_B_LE:
+			opt = i0_ins_opt_pref_b_le;
+			break;
+		case I0_OPT_B_E:
+			opt = i0_ins_opt_pref_b_e;
+			break;
+		case I0_OPT_B_L:
+			opt = i0_ins_opt_pref_b_l;
+			break;
+		case I0_OPT_B_NE:
+			opt = i0_ins_opt_pref_b_ne;
+			break;
+		case I0_OPT_B_SL:
+			opt = i0_ins_opt_pref_b_sl;
+			break;
+		case I0_OPT_B_Z:
+			opt = i0_ins_opt_pref_b_z;
+			break;
+		case I0_OPT_B_NZ:
+			opt = i0_ins_opt_pref_b_nz;
+			break;
+		default:
+			throw(int(I0_DECODE_STATUS_B_OPT));
+		}
+		switch (b_opt) {
+		case I0_OPT_B_LE:
+		case I0_OPT_B_E:
+		case I0_OPT_B_L:
+		case I0_OPT_B_NE:
+		case I0_OPT_B_SL:
+			op_name = I0_ins_bcc;
+			opcode4.load_extra_bytes(I0_INS_LEN_BCMP);
+			cmp_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+			Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+			Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+			opt = opcode4.fetch_bits(I0_INS_BIT_LEN_RA);
+			fill_oper(Op1);
+			fill_oper(Op2);
+			fill_oper_C(Op3);
+			return;
+		case I0_OPT_B_Z:
+		case I0_OPT_B_NZ:
+			op_name = I0_ins_bcz;
+			opcode4.load_extra_bytes(I0_INS_LEN_BZNZ);
+			cmp_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+			Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+			opt = opcode4.fetch_bits(I0_INS_BIT_LEN_RA);
+			fill_oper(Op1);
+			fill_oper_C(Op2);
+			return;
+		default:
+			throw(int(0));
+		}
+	}
+		return;
+	case I0_OPCODE_NOP:
+		op_name = I0_ins_nop;
+		opcode4.load_extra_bytes(I0_INS_LEN_NOP);
+		return;
+	case I0_OPCODE_INT:
+		op_name = I0_ins_int;
+		opcode4.load_extra_bytes(I0_OPCODE_INT);
+		Op1.addrm=I0_ADDRM_IMMEDIATE;
+		fill_oper(Op1, I0_ATTR_UB);
+		return;
+	case I0_OPCODE_SHIFT:
+		opcode4.load_extra_bytes(I0_INS_LEN_SHIFT);
+		opt = opcode4.fetch_bits(I0_INS_BIT_LEN_OPT_SHIFT);
+		// currently other 2 options not implemented
+		switch(opt)
+		{
+		case I0_OPT_SHIFT_L:
+			op_name = I0_ins_shl;
+			break;
+		case I0_OPT_SHIFT_R:
+			op_name = I0_ins_shr;
+			break;
+		default:
+			throw(int(I0_DECODE_STATUS_SHRL_OPT));
+		}
+		ins_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+		Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op3.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		fill_oper(Op1);
+		fill_oper(Op2);
+		fill_oper_M(Op3);
+		return;
+	case I0_OPCODE_SCMP:
+		op_name = I0_ins_scmp;
+		opcode4.load_extra_bytes(I0_INS_LEN_SCMP);
+		Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op3.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op4.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op5.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		ins_attr = I0_ATTR_UE;
+		fill_oper_M(Op1);
+		fill_oper(Op2);
+		fill_oper_M(Op3);
+		fill_oper(Op4);
+		fill_oper_M(Op5);
+		return;
+	default:
+		throw(int(I0_DECODE_STATUS_OPCODE));
+	}
+	//only alu instructions fall here!
+	switch (opcode) {
+	case I0_OPCODE_ADD:
+	case I0_OPCODE_SUB:
+	case I0_OPCODE_MUL:
+	case I0_OPCODE_DIV:
+	case I0_OPCODE_AND:
+	case I0_OPCODE_OR:
+	case I0_OPCODE_XOR:
+		opcode4.load_extra_bytes(I0_INS_LEN_ALU);
+		ins_attr = opcode4.fetch_bits(I0_INS_BIT_LEN_ATTR);
+		Op1.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op2.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		Op3.addrm = opcode4.fetch_bits(I0_INS_BIT_LEN_ADDRM);
+		fill_oper(Op1);
+		fill_oper(Op2);
+		fill_oper_M(Op3);
+		return;
+	default:
+		throw(int(0));
+	}
+}
+catch (...) {
+	delete[] opnds;
+	throw;
+}
+
+insn_t::~insn_t() {
+	delete[] opnds;
+}
+uint8_t insn_t::i0_fetch_byte() {
+	uint8_t ret = i0_get_byte(ip + size);
+	size += sizeof(uint8_t);
+	return ret;
+}
+uint16_t insn_t::i0_fetch_word() {
+	uint16_t ret = i0_get_word(ip + size);
+	size += sizeof(uint16_t);
+	return ret;
+}
+uint32_t insn_t::i0_fetch_dword() {
+	uint32_t ret = i0_get_dword(ip + size);
+	size += sizeof(uint32_t);
+	return ret;
+}
+uint64_t insn_t::i0_fetch_qword() {
+	uint64_t ret = i0_get_qword(ip + size);
+	size += sizeof(uint64_t);
+	return ret;
+}
+
+int main(int argc, char** argv) {
 	int i0_prog_fd = open(argv[1], O_RDONLY);
-	if(i0_prog_fd<0)
-	{
+	if (i0_prog_fd < 0) {
 		exit(-1);
 	}
 	struct stat file_info;
 	fstat(i0_prog_fd, &file_info);
 	__off64_t i0_prog = file_info.st_size;
-	void* mapped_text_seg = mmap(NULL, i0_prog,PROT_READ, MAP_PRIVATE,i0_prog_fd,0);
-	while(1)
-	{
+	void* mapped_text_seg = mmap(NULL, i0_prog, PROT_READ, MAP_PRIVATE,
+			i0_prog_fd, 0);
 
-	}
 	return 0;
 }
---
+++ src/scan_func.hpp
@@ -0,0 +1,257 @@
+#ifndef SCAN_FUNC_H
+#define SCAN_FUNC_H
+
+typedef uint64_t i0_ea_type_t;
+typedef uint64_t i0_val_type_t;
+
+#define qnumber(a)     (sizeof(a)/sizeof((a)[0]))
+
+#define I0_DECODE_STATUS_B_OPT 				(1U)
+#define I0_DECODE_STATUS_SHRL_OPT 			(2U)
+#define I0_DECODE_STATUS_OPCODE				(3U)
+#define I0_MAX_OPND_NUMBER	5
+
+enum I0_ins_options {
+	i0_ins_opt_pref_b_le,
+	i0_ins_opt_pref_b_l,
+	i0_ins_opt_pref_b_e,
+	i0_ins_opt_pref_b_ne,
+	i0_ins_opt_pref_b_sl,
+	i0_ins_opt_pref_b_z,
+	i0_ins_opt_pref_b_nz,
+	i0_ins_opt_pref_exit_c,
+	i0_ins_opt_pref_exit_a,
+	i0_ins_opt_pref_exit_cd,
+	i0_ins_opt_pref_exit_ad,
+	i0_ins_opt_pref_last
+};
+
+enum i0_ins_names {
+	I0_ins_conv,
+	I0_ins_add,
+	I0_ins_sub,
+	I0_ins_mul,
+	I0_ins_div,
+	I0_ins_and,
+	I0_ins_or,
+	I0_ins_xor,
+	I0_ins_spawn,
+	I0_ins_spawnx,
+	I0_ins_exit,
+	I0_ins_bcc,
+	I0_ins_bcz,
+	I0_ins_bj,
+	I0_ins_bij,
+	I0_ins_nop,
+	I0_ins_int,
+	I0_ins_shl,
+	I0_ins_shr,
+	I0_ins_scmp,
+	I0_ins_mov,
+	I0_ins_last_ins
+};
+
+enum i0_instr_addrm {
+	i0_addrm_Imm = 0,
+	i0_addrm_Abs = 1,
+	i0_addrm_Indir = 2,
+	i0_addrm_Disp = 3,
+	i0_addrm_last
+};
+
+enum i0_oper_attr {
+	i0_attr_sb = 0,
+	i0_attr_se = 1,
+	i0_attr_ss = 2,
+	i0_attr_sf = 3,
+	i0_attr_ub = 4,
+	i0_attr_ue = 5,
+	i0_attr_us = 6,
+	i0_attr_uf = 7,
+	i0_attr_fs = 8,
+	i0_attr_fd = 9,
+	i0_attr_last
+};
+
+enum I0_regs {
+	i0_reg_BP,
+	i0_reg_SP,
+	i0_reg_R2,
+	i0_reg_R3,
+	i0_reg_R4,
+	i0_reg_R5,
+	i0_reg_R6,
+	i0_reg_R7,
+	i0_reg_rVcs,
+	i0_reg_rVds,
+	i0_reg_l0_stdin,
+	i0_reg_l0_stdout,
+	i0_reg_TaskWpr_sp,
+	i0_reg_Syscall_sp,
+	i0_reg_rrStkBase,
+	i0_reg_rrStkLen,
+	i0_reg_rr_fi,
+	i0_reg_rr_ID,
+	i0_reg_I0_regs_last
+};
+
+struct op_mem_desp{
+	int32_t displ;
+	i0_ea_type_t addr;
+};
+
+struct op_t {
+	//uint8_t attr;
+	uint8_t addrm;
+	uint8_t ins_offset;
+	/*bool code_ref;
+	int32_t displ;
+	union {
+		i0_ea_type_t addr;
+		uint8_t reg;
+		uint64_t val_ue;
+		int64_t val_se;
+		uint32_t val_uf;
+		int32_t val_sf;
+		uint8_t val_ub;
+		int8_t val_sb;
+		float val_fs;
+		double val_fd;
+	};*/
+};
+
+static_assert((sizeof(op_t) == (1+1+1+1+4+8)), "check op_t size");
+
+class insn_t;
+class i0_opcode4_t {
+private:
+	i0_opcode4_t();
+	uint32_t get_bits(unsigned start, unsigned len);
+	insn_t& insn_ref;
+	union {
+		uint8_t b[4];
+		uint32_t l;
+	} opcode4;
+	unsigned shift_cnt;
+	void load_bytes(uint8_t cnt);
+	void load_op_bytes();
+public:
+	i0_opcode4_t(insn_t& _cmd);
+	void load_extra_bytes(uint8_t cnt);
+	uint32_t fetch_bits(unsigned len);
+};
+
+class insn_t {
+public:
+	insn_t(i0_ea_type_t _ip);
+	~insn_t();
+private:
+	insn_t();
+	void fill_oper(op_t&);
+	void fill_oper(op_t& op,uint8_t attr);
+	void fill_oper_M(op_t&);
+	void fill_oper_M(op_t& op, uint8_t attr);
+	void fill_oper_C(op_t& op);
+	void fill_oper_C_indir(op_t& op);
+	uint8_t i0_fetch_byte();
+	uint16_t i0_fetch_word();
+	uint32_t i0_fetch_dword();
+	uint64_t i0_fetch_qword();
+	void i0_check_byte();
+	void i0_check_word();
+	void i0_check_dword();
+	void i0_check_qword();
+	i0_ea_type_t ip;
+	i0_ins_names op_name;
+	I0_ins_options opt;
+	op_t opnds[I0_MAX_OPND_NUMBER];
+	union {
+		uint8_t ins_attr;
+		uint8_t src_attr;
+		uint8_t cmp_attr;
+	};
+	union {
+		uint8_t __ins_extra_attr__;
+		uint8_t dest_attr;
+	};
+	uint8_t size;
+	friend class i0_opcode4_t;
+};
+
+#define I0_OPCODE_NOP		(0x00U)
+#define I0_OPCODE_ADD		(0x03U)
+#define I0_OPCODE_SUB		(0x06U)
+#define I0_OPCODE_MUL		(0x09U)
+#define I0_OPCODE_DIV		(0x0cU)
+#define I0_OPCODE_B			(0x15U)
+#define I0_OPCODE_AND		(0x18U)
+#define I0_OPCODE_OR		(0x1bU)
+#define I0_OPCODE_XOR		(0x1eU)
+#define I0_OPCODE_CONV		(0x21U)
+#define I0_OPCODE_INT		(0x24U)
+#define I0_OPCODE_SPAWN		(0x25U)
+#define I0_OPCODE_SHIFT		(0x27U)
+#define I0_OPCODE_SCMP		(0x2aU)
+#define I0_OPCODE_EXIT		(0x86U)
+
+#define I0_OPT_JUMP_R		(0x01U)
+#define I0_OPT_JUMP_A		(0x00U)
+#define I0_OPT_EXIT_C		(0x00U)
+#define I0_OPT_EXIT_A		(0x01U)
+#define I0_OPT_EXIT_CD		(0x02U)
+#define I0_OPT_EXIT_AD		(0x03U)
+#define I0_OPT_B_J			(0x00U)
+#define I0_OPT_B_L			(0x02U)
+#define I0_OPT_B_LE			(0x03U)
+#define I0_OPT_B_E			(0x04U)
+#define I0_OPT_B_NE			(0x05U)
+#define I0_OPT_B_Z			(0x06U)
+#define I0_OPT_B_NZ			(0x07U)
+#define I0_OPT_B_SL			(0x08U)
+#define I0_OPT_B_IJ			(0x0cU)
+#define I0_OPT_SHIFT_L		(0x00U)
+#define I0_OPT_SHIFT_R		(0x01U)
+
+#define I0_INS_LEN_CONV		(0x04U)
+#define I0_INS_LEN_ALU		(0x03U)
+#define I0_INS_LEN_SPAWN	(0x03U)
+#define I0_INS_LEN_EXIT		(0x02U)
+#define I0_INS_LEN_BIJ		(0x03U)
+#define I0_INS_LEN_BJ		(0x02U)
+#define I0_INS_LEN_BCMP		(0x04U)
+#define I0_INS_LEN_BZNZ		(0x03U)
+#define I0_INS_LEN_NOP		(0x02U)
+#define I0_INS_LEN_INT		(0x02U)
+#define I0_INS_LEN_SHIFT	(0x04U)
+#define I0_INS_LEN_SCMP		(0x04U)
+#define I0_INS_LEN_OPCODE	(0x02U)
+
+#define I0_INS_BIT_LEN_OPCODE			(0x0bU)
+#define I0_INS_BIT_LEN_OPT_EXIT			(0x02U)
+#define I0_INS_BIT_LEN_OPT_B			(0x04U)
+#define I0_INS_BIT_LEN_RA				(0x01U)
+#define I0_INS_BIT_LEN_MATTR			(0x04U)
+#define I0_INS_BIT_LEN_ATTR				(0x04U)
+#define I0_INS_BIT_LEN_ADDRM			(0x03U)
+#define I0_INS_BIT_LEN_OPT_SHIFT		(0x02U)
+
+#define I0_INS_LEN_ADDRM_LEN		(3U)
+#define I0_INS_LEN_ATTR_LEN			(4U)
+
+#define I0_ADDRM_IMMEDIATE			(0x00U)
+#define I0_ADDRM_ABSOLUTE			(0x01U)
+#define I0_ADDRM_INDIRECT			(0x02U)
+#define I0_ADDRM_DISPLACEMENT		(0x03U)
+
+#define I0_ATTR_SB					(0x00U)
+#define I0_ATTR_SE					(0x01U)
+#define I0_ATTR_SS					(0x02U)
+#define I0_ATTR_SF					(0x03U)
+#define I0_ATTR_UB					(0x04U)
+#define I0_ATTR_UE					(0x05U)
+#define I0_ATTR_US					(0x06U)
+#define I0_ATTR_UF					(0x07U)
+#define I0_ATTR_FS					(0x08U)
+#define I0_ATTR_FD					(0x09U)
+
+#endif
